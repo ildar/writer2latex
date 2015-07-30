d207d29034d43f7dbf124ddf09a86e6684b07891
henrikjust <henrikjust@f0f2a975-2e09-46c8-9428-3b39399b9f3c>
2015-07-30 11:32:25 +0000

    w2l: Enable export from WMF/EMF to PDF
    git-svn-id: https://svn.code.sf.net/p/writer2latex/code/trunk@266 f0f2a975-2e09-46c8-9428-3b39399b9f3c

--- source/java/org/openoffice/da/comp/w2lcommon/filter/GraphicConverterImpl2.java
+++ source/java/org/openoffice/da/comp/w2lcommon/filter/GraphicConverterImpl2.java
@@ -16,11 +16,11 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  *  MA  02111-1307  USA
  *
- *  Copyright: 2002-2012 by Henrik Just
+ *  Copyright: 2002-2015 by Henrik Just
  *
  *  All Rights Reserved.
  * 
- *  Version 1.4 (2012-04-03)
+ *  Version 1.6 (2015-07-30)
  *
  */
  
@@ -103,8 +103,8 @@ public class GraphicConverterImpl2 implements GraphicConverter {
         boolean bSupportsSource = MIMETypes.SVM.equals(sSourceMime) ||
            MIMETypes.PNG.equals(sSourceMime) || MIMETypes.JPEG.equals(sSourceMime) ||
            MIMETypes.GIF.equals(sSourceMime) || MIMETypes.TIFF.equals(sSourceMime) ||
-           MIMETypes.BMP.equals(sSourceMime);
-           //|| MIMETypes.WMF.equals(sSourceMime) || MIMETypes.EMF.equals(sSourceMime);
+           MIMETypes.BMP.equals(sSourceMime)
+           || MIMETypes.WMF.equals(sSourceMime) || MIMETypes.EMF.equals(sSourceMime);
         boolean bSupportsTarget = MIMETypes.PDF.equals(sTargetMime) || MIMETypes.EPS.equals(sTargetMime) ||
         	MIMETypes.SVG.equals(sTargetMime);
         return bSupportsSource && bSupportsTarget;
