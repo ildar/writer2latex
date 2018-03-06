35e8bb0c1db1ce2d31f8d172de3a506635288065
henrikjust <henrikjust@f0f2a975-2e09-46c8-9428-3b39399b9f3c>
2018-03-06 20:06:05 +0000

    Change license to GPLv3
    git-svn-id: https://svn.code.sf.net/p/writer2latex/code/trunk@272 f0f2a975-2e09-46c8-9428-3b39399b9f3c

--- source/java/org/openoffice/da/comp/writer2latex/TeXDetectService.java
+++ source/java/org/openoffice/da/comp/writer2latex/TeXDetectService.java
@@ -2,23 +2,22 @@
  *
  *  TeXDetectService.java
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License version 2.1, as published by the Free Software Foundation.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
- *
  *  Copyright: 2002-2014 by Henrik Just
  *
- *  All Rights Reserved.
+ *  This file is part of Writer2LaTeX.
+ *  
+ *  Writer2LaTeX is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  Writer2LaTeX is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with Writer2LaTeX.  If not, see <http://www.gnu.org/licenses/>.
  * 
  *  Version 1.6 (2014-10-29)
  *
